a12672ac3488c0f96255fd4ebd9a4dd6042a0a7d
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-03-07 14:51:04 +0700

    context

--- context_test.go
+++ context_test.go
@@ -3,125 +3,123 @@ package golang_context
 import (
 	"context"
 	"fmt"
-	"runtime"
+	"sync"
 	"testing"
 	"time"
 )
 
+// Context biasanya dibuat per request(misal setiap ada request masuk ke server melalui http request), context digunakan untuk mempermudah kita meneruskan value, dan sinyal antar proses.
+// Context di golang biasa digunakan untuk mengirim data request atau sinyal ke proses lain atau dari gorutine ke goroutine yang lain. Dengan menggunakan context, ketika kita ingin membatalkan sebuah proses, kita cukup mengirim sinyal ke context nya, maka secara otomatis semua proses akan dibatalkan.
+
 func TestContext(t *testing.T) {
+	// Membuat context kosong. Tidak pernah dibatalkan, tidak pernah timeout, dam tidak memiliki value apapun. Biasanya digunakan di main function atau dalam test, atau dalam awal request proses terjadi.
 	background := context.Background()
 	fmt.Println(background)
 
+	// Membuat context kosong seperti Background(), namun biasanya menggunakan ini ketika belum jelas context apa yang ingin digunakan.
 	todo := context.TODO()
 	fmt.Println(todo)
 }
 
+// Context menganut konsep parent dan child.
+// saat kita membuat context, kita bisa membuat child context dari context yang sudah ada.
+
+// Pada saat awal membuat context, context tidak memiliki value. Kita bisa menambahkan sebuah value dengan data Pair (key – value) ke dalam context, saat kita menambahkan value ke context secara otomatis akan tercipta child context baru
+// original context nya tidak akan berubah sama sekali. Untuk menambahkan value ke context kita bisa menggunakan function context.WithValue(parent, key, value).
+
 func TestContextWithValue(t *testing.T) {
-	contextA := context.Background()
+	parent := context.Background()
 
-	contextB := context.WithValue(contextA, "b", "B")
-	contextC := context.WithValue(contextA, "c", "C")
+	childA := context.WithValue(parent, "Key", "Muhammad")
+	childB := context.WithValue(childA, "Key", "Deril")
 
-	contextD := context.WithValue(contextB, "d", "D")
-	contextE := context.WithValue(contextB, "e", "E")
+	fmt.Println(childA)
+	fmt.Println(childB)
 
-	contextF := context.WithValue(contextC, "f", "F")
-	contextG := context.WithValue(contextF, "g", "G")
-
-	fmt.Println(contextA)
-	fmt.Println(contextB)
-	fmt.Println(contextC)
-	fmt.Println(contextD)
-	fmt.Println(contextE)
-	fmt.Println(contextF)
-	fmt.Println(contextG)
-
-	fmt.Println(contextF.Value("f"))
-	fmt.Println(contextF.Value("c"))
-	fmt.Println(contextF.Value("b"))
-
-	fmt.Println(contextA.Value("b"))
+	// mengambil value nya dan menampilkan ke layar
+	fmt.Println(childA.Value("Key"))
+	fmt.Println(childB.Value("Key"))
 }
 
-func CreateCounter(ctx context.Context) chan int {
-	destination := make(chan int)
+//  goroutine yang menggunakan context tetap harus melakukan pengecekan terhadap context nya, jika tidak maka tidak ada gunanya
+//  Untuk membuat context dengan cancel signal kita bisa menggunakan function context.WithCancel(parent)
 
-	go func() {
-		defer close(destination)
-		counter := 1
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				destination <- counter
-				counter++
-				time.Sleep(1 * time.Second) // simulasi slow
-			}
-		}
-	}()
+func Satu(ctx context.Context, cancel func(), group *sync.WaitGroup) {
+	defer group.Done()
+	canceled := ctx.Err()
 
-	return destination
+	fmt.Println("Goroutine 1 start")
+	if canceled != nil {
+		fmt.Println(canceled)
+		return
+	}
+	fmt.Println("Goroutin 1 done")
+	cancel()
+}
+
+func Dua(ctx context.Context, cancel func(), group *sync.WaitGroup) {
+	defer group.Done()
+	fmt.Println("Goroutine 2 start")
+
+	// membuat goroutine 2 lebih lambat dengan menambahkan Sleep selama 1 detik.
+	time.Sleep(1 * time.Second)
+	canceled := ctx.Err()
+
+	if canceled != nil {
+		fmt.Println("Goroutine 2", canceled)
+		return
+	}
+	fmt.Println("Goroutin 2 done")
+	cancel()
 }
 
 func TestContextWithCancel(t *testing.T) {
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
-
+	group := sync.WaitGroup{}
 	parent := context.Background()
-	ctx, cancel := context.WithCancel(parent)
+	data, cancel := context.WithCancel(parent)
+	ctx := context.WithValue(data, "data", "Muhammad Deril")
 
-	destination := CreateCounter(ctx)
-
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
-
-	for n := range destination {
-		fmt.Println("Counter", n)
-		if n == 10 {
-			break
-		}
-	}
-
-	cancel() // mengirim sinyal cancel ke context
-
-	time.Sleep(2 * time.Second)
-
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+	fmt.Println("Start 2 goroutine")
+	group.Add(2)
+	go Satu(ctx, cancel, &group)
+	go Dua(ctx, cancel, &group)
+	group.Wait()
 }
+
+// Selain menambahkan value ke context, dan juga sinyal cancel, kita juga bisa menambahkan sinyal cancel ke context secara otomatis dengan pengaturan timeout.
+// Untuk membuat context dengan cancel signal secara otomatis menggunakan timeout, kita bisa menggunakan function context.WithTimeout(parent, duration).
 
 func TestContextWithTimeout(t *testing.T) {
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
-
-	parent := context.Background()
-	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	destination := CreateCounter(ctx)
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
-
-	for n := range destination {
-		fmt.Println("Counter", n)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("overslept")
+		}
 	}
-
-	time.Sleep(2 * time.Second)
-
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
 
+// Selain menggunakan timeout untuk melakukan cancel secara otomatis, kita juga bisa menggunakan deadline.
+// Pengaturan deadline sidikit berbeda dengan timeout, jika timeout kita beri waktu dari sekarang, nah jika deadline ditentukan kapan waktu timeout nya, misal jam 12 siang hari ini.
+
 func TestContextWithDeadline(t *testing.T) {
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 
-	parent := context.Background()
-	ctx, cancel := context.WithDeadline(parent, time.Now().Add(5*time.Second))
+	// Untuk penggunaan deadline sebenar nya sama saja dengan timeout sebelumnya, yang berbeda hanya method dan parameter nya saja
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(5*time.Second))
 	defer cancel()
-
-	destination := CreateCounter(ctx)
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
-
-	for n := range destination {
-		fmt.Println("Counter", n)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("overslept")
+		}
 	}
-
-	time.Sleep(2 * time.Second)
-
-	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
